56ba15a95a04517ee7df0595e984635379aa7138
Shahin Pashayev <shahin.pashayev@gmail.com>
2026-02-13 00:36:32 +0400

    Commit

---
+++ main_lessons/main_lesson2.py
@@ -0,0 +1,9 @@
+blacklist_dict = {"silah": 0.95, "qan": 0.83, "axmaq": 0.75,  }
+
+user_prompt = input("sorgunuzu daxil edin: ")
+
+if user_prompt in blacklist_dict:
+    print("Daxil elediyiniz soz qadağandır")
+else:
+    print("ugurlu sorğu")
+
