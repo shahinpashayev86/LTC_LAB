de64d0fea127c9f986facec2ed77fd99c17692e9
spashayev <shahin.pashayev@gmail.com>
2026-02-13 09:52:16 +0400

    commit

--- main_lessons/main_lesson2.py
+++ main_lessons/main_lesson2.py
@@ -1,4 +1,4 @@
-blacklist_dict = {"silah": 0.95, "qan": 0.83, "axmaq": 0.75,  }
+blacklist_dict = {"silah": 0.95, "qan": 0.83, "bomba": 0.75, "axmaq": 0.5  }
 
 user_prompt = input("sorgunuzu daxil edin: ")
 
